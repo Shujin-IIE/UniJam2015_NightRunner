ce6252a7c0e1dbff5ee28f554af97567424d4407
Shujin <cedric.pignard-fernandez@ensiie.fr>
2015-11-21 22:44:17 +0100

    Add LightManager…

---
+++ Assets/Scripts/Light Manager/LightManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightManager : MonoBehaviour {
+	[SerializeField]
+	private float intensityMax = 0;
+
+	[SerializeField]
+	private float deltaDiminishTime = 0;
+	private float lastDiminish = 0;
+
+	[SerializeField]
+	private float diminishValue = 0;
+
+	private Light light;
+
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light>();
+
+		if (light == null) {
+			Debug.LogWarning("LightManager.Start(): light == null");
+		}
+
+		if (intensityMax < 0 || intensityMax > 8) {
+			Debug.LogWarning("LightManager.Start(): intensityMax out of bound (intensityMax = " + intensityMax + " )");
+		}
+
+		light.intensity = intensityMax;
+		lastDiminish = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DiminishIntensityByTime();
+	}
+
+	void DiminishIntensityByTime () {
+		if (lastDiminish + deltaDiminishTime < Time.time) {
+			ModifyIntensity(-diminishValue);
+
+			lastDiminish = Time.time;
+			if (light.intensity <= 0) {
+				// TODO: Die?
+			}
+		}
+	}
+
+	public void ModifyIntensity (float value) {
+		light.intensity += value;
+	}
+}
